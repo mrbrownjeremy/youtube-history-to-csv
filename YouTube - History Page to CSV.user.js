f070aec40793a16cbb4300b073d529cd949b0316
Jeremy Brown <1113491+mrbrownjeremy@users.noreply.github.com>
2026-05-19 17:53:24 -0400

    Initial commit: blank script scaffold

---
+++ YouTube - History Page to CSV.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         YouTube - History Page to CSV
+// @namespace    https://github.com/mrbrownjeremy
+// @version      1.0.0
+// @description  Adds a "Download Visible to CSV" button to the YouTube history page
+// @author       Jeremy Brown
+// @match        https://www.youtube.com/feed/history*
+// @icon         data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCA2NCA2NCI+CiAgPHJlY3Qgd2lkdGg9IjY0IiBoZWlnaHQ9IjY0IiByeD0iMTIiIGZpbGw9IiNGRjAwMzQiLz4KICA8cGF0aCBkPSJNMzIgMTZWNDAgTTIyIDMwbDEwIDEwIDEwLTEwIiBzdHJva2U9IndoaXRlIiBzdHJva2Utd2lkdGg9IjUiIHN0cm9rZS1saW5lY2FwPSJyb3VuZCIgc3Ryb2tlLWxpbmVqb2luPSJyb3VuZCIgZmlsbD0ibm9uZSIvPgogIDxsaW5lIHgxPSIxNiIgeTE9IjQ4IiB4Mj0iNDgiIHkyPSI0OCIgc3Ryb2tlPSJ3aGl0ZSIgc3Ryb2tlLXdpZHRoPSI1IiBzdHJva2UtbGluZWNhcD0icm91bmQiLz4KPC9zdmc+Cg==
+// @grant        none
+// ==/UserScript==
+
+(function () {
+  'use strict';
+
+})();
